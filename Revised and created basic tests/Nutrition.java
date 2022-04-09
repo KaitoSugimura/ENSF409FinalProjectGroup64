46ed8086b04ed146d676da0c6a25b82df29cb9a2
HecticKiwi <www.dannyduong123@hotmail.com>
2022-04-09 09:24:11 -0600

    Added comments, commented out total calories

--- Revised and created basic tests/Nutrition.java
+++ Revised and created basic tests/Nutrition.java
@@ -5,7 +5,7 @@ public class Nutrition {
     private final int FRUIT_VEGGIES;
     private final int PROTEIN;
     private final int OTHER;
-    private final int CALORIES;
+    // private final int CALORIES;
 
     public Nutrition(ClientType bodyType){
         Database database = new Database("jdbc:mysql://localhost/food_inventory", "student", "ensf");
@@ -18,21 +18,23 @@ public class Nutrition {
         
         int[] nutValues = database.getClientValues(bodyType.toString());
         
+        // Calculate values from the percentage and total calories
         this.WHOLE_GRAINS = (int)Math.ceil(nutValues[0] / 100.0 * nutValues[4]);
         this.FRUIT_VEGGIES = (int)Math.ceil(nutValues[1] / 100.0 * nutValues[4]);
         this.PROTEIN = (int)Math.ceil(nutValues[2] / 100.0 * nutValues[4]);
         this.OTHER = (int)Math.ceil(nutValues[3] / 100.0 * nutValues[4]);
-        this.CALORIES = nutValues[4];
-
+        // this.CALORIES = nutValues[4];
+        
         database.close();
     }
-
+    
     public Nutrition(int wholeGrains, int fruitVeggies, int protein, int other, int calories){
+        // Calculate values from the percentage and total calories
         this.WHOLE_GRAINS = (int)Math.ceil(wholeGrains / 100.0 * calories);
         this.FRUIT_VEGGIES = (int)Math.ceil(fruitVeggies / 100.0 * calories);
         this.PROTEIN = (int)Math.ceil(protein / 100.0 * calories);
         this.OTHER = (int)Math.ceil(other / 100.0 * calories);
-        this.CALORIES = calories;
+        // this.CALORIES = calories;
     }
 
     public int getWholeGrains(){
@@ -51,13 +53,12 @@ public class Nutrition {
         return this.OTHER;
     }
 
-    public int getCalories(){
-        return this.CALORIES;
-    }
+    // public int getCalories(){
+    //     return this.CALORIES;
+    // }
 
     public static void main(String[] args) {
         Nutrition test = new Nutrition(ClientType.ADULT_MALE);
-        System.out.println(test.getCalories());
         System.out.println(test.getProtein());
     }
 }
