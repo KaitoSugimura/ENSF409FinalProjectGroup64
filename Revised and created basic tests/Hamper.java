8307c06a9325ff49e9468c8758e2fa5808eba79e
JoshuaW13 <josh.walters@shaw.ca>
2022-04-09 11:36:35 -0600

    Hmaper documentation

--- Revised and created basic tests/Hamper.java
+++ Revised and created basic tests/Hamper.java
@@ -1,23 +1,47 @@
+/**
+@author Danny Duong, Kaito Sugimura, Kevin Johnson, Joshua Walters
+@version 1.9
+@since 1.0
+*/
+
+/*
+class Hamper - contains data for a given order's hampers.
+*/
 import java.util.*;
 
 public class Hamper {
+    //Fields
     private ArrayList<FoodItem> foodItems = new ArrayList<>();
     private ArrayList<Client> clients = new ArrayList<>();
 
+    //Public Methods
+    /**
+     * resetItems() - resets the food items contained within the hamper.
+     */
     public void resetItems() {
         this.foodItems = new ArrayList<>();
     }
 
+    /**
+     * setItems() - update the fooditems arraylist.
+     * @param items
+     */
     public void setItems(ArrayList<FoodItem> items){
         this.foodItems = items;
     }
 
+    /**
+     * addClient() - add a new client to the current hamper.
+     * @param bodyType
+     * @param quantity
+     */
     public void addClient(ClientType bodyType, int quantity){
         for(int i = 0; i < quantity; i++){
             this.clients.add(new Client(bodyType));
         }
     }
 
+    //getters
     public ArrayList<FoodItem> getItems(){
         return this.foodItems;
     }
