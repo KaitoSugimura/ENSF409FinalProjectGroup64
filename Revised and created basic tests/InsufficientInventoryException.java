332661c16011f59cb2e751c5a9a80e3bb003b860
JoshuaW13 <josh.walters@shaw.ca>
2022-04-09 11:41:56 -0600

    exceptions documentaiton

--- Revised and created basic tests/InsufficientInventoryException.java
+++ Revised and created basic tests/InsufficientInventoryException.java
@@ -1,8 +1,25 @@
+/**
+@author Danny Duong, Kaito Sugimura, Kevin Johnson, Joshua Walters
+@version 1.9
+@since 1.0
+*/
+
+/*
+class InsufficientInventoryException - throws an exception for cases when there are not enough
+items in the inventory for the attempted operations.
+*/
 public class InsufficientInventoryException extends Exception {
+    //public methods
+    /**
+     * InsufficientInventoryException() - prints exception with message
+     */
     public InsufficientInventoryException(){
         super("Insufficient resources in Inventory");
     }
 
+    /**
+     * InsufficientInventoryException() - prints exception with custom message
+     */
     public InsufficientInventoryException(String message){
         super(message);
     }
