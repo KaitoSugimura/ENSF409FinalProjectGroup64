a87fb7ad544a11a05e9a1f15b73794af3d834e55
JoshuaW13 <kingdraganoid@gmail.com>
2022-04-12 11:07:34 -0600

    OrderForm Test updated
    Incomplete as second test is busted

--- FINAL2/OrderFormTest.java
+++ FINAL2/OrderFormTest.java
@@ -1,14 +1,15 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.ArrayList;
+import java.io.FileReader;
+import java.util.Scanner;
 public class OrderFormTest {
-    ArrayList<Hamper> myhampers = new ArrayList<>();
+    
 
     @Before
     public void setUp() {
-
         Application testApp = new Application();
         testApp.addHamper();
         testApp.addClient(0, ClientType.ADULT_FEMALE, 2);
@@ -19,7 +20,9 @@ public class OrderFormTest {
             
         } catch (Exception e) {
             System.out.println("An exception occured during the test setup!");
-        }   
+        }  
+
+        
     }
 
     /* OrderForm TESTS */
@@ -27,7 +30,6 @@ public class OrderFormTest {
     //Creates order form txt file
     @Test
     public void testOrderFormTextFileCreated(){
-        //OrderForm.printOrder(myhampers);
         
         File f = new File("OrderForm.txt");
         boolean expected = true;
@@ -35,5 +37,32 @@ public class OrderFormTest {
         assertEquals("File was not created as expected",expected, actual);
 
     }
+
+    @Test
+    public void testOrderFormFormat(){
+        String expected = "64 Food Bank\nHamper Order Form\n\nName:\nDate:\n\nOriginal Request\n\nHamper1: 2 Adult Females, \n\nHamper 1 Items:\n1\tTuna, six large cans\n1\tChicken breast, pound\n2\tBroccoli, 3 bunches\n1\tGranola Bar, box\n1\tChicken broth, can"; 
+        expected.trim();
+        String actual="";
+        String st = "";
+        
+        
+        try {
+            File file = new File("OrderForm.txt");
+            Scanner myReader = new Scanner(file);
+            while((st =myReader.nextLine())!= null){
+                actual += st+"\n";
+            }
+            myReader.close();
+            
+                       
+        } catch (Exception e) {
+            System.out.println("File error while running the test!");
+        }
+        System.out.println();
+        expected.trim();
+        actual.trim();
+        assertEquals("File was not printed in the expected format!",expected, actual);
+    }
+    
     
 }
