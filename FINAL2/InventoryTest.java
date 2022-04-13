0602382e7bde354185f4e84218057d835845cdb7
JoshuaW13 <kingdraganoid@gmail.com>
2022-04-12 22:35:32 -0600

    Documentation updated

--- FINAL2/InventoryTest.java
+++ FINAL2/InventoryTest.java
@@ -1,3 +1,8 @@
+/**
+@author Danny Duong, Kaito Sugimura, Kevin Johnson, Joshua Walters
+@version 2.1
+@since 1.0
+*/
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.ArrayList;
